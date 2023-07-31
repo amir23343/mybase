3191ebc0447a641c4b96a117610bf1ce5ac27836
amir <88289436+amir23343@users.noreply.github.com>
2023-07-31 12:09:20 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x9e31b23de8f104bae739e1278baa098179c8fc6f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
